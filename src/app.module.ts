5f1020517752f674d20018ef7d2a0cfc7ad34abb
ftoledo43 <franciscojaviertoledocollante@gmail.com>
2024-12-28 11:51:49 -0300

    preparar mongodb y envs 4

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,9 @@ import { JoiValidationSchema } from './config/joi.validation';
     }),
 
     MongooseModule.forRoot(process.env.MONGODB, {
-      dbName: 'pokemonsDB'
+      dbName: 'pokemonsDB',
+      retryWrites: true,
+      w: 'majority'
     }),
 
     PokemonModule,
